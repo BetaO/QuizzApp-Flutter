a38233876673e0ad0089be34fa53852b95975405
Oualid Betari <beta.oualid@gmail.com>
2023-12-01 14:03:14 +0100

    Added Start screen (v2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
-import 'package:adv_basics/myapp.dart';
+import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 25, 0, 65),
+                Color.fromARGB(255, 0, 110, 200),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  void startQuiz() {
+    print('Start Quiz');
+  }
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 68, 121),
+              foregroundColor: Colors.white,
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
