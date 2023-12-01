c0397d482dd3e1603d587faf4e0a8a9016b09354
Oualid Betari <beta.oualid@gmail.com>
2023-12-01 15:27:40 +0100

    Created Quiz and QuestionsScrren custom widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromARGB(255, 25, 0, 65),
-                Color.fromARGB(255, 0, 110, 200),
-              ],
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
